1d7c37b2eeabe7d186373d4dd9805df91fbd8da4
Florent FREMONT <ff.fremont.florent@gmail.com>
2016-10-02 08:43:39 +0200

    édition web de la conf + doc

--- README.md
+++ README.md
@@ -3,18 +3,42 @@ Le programme downloader permet de télécharger des fichiers en automatique sans
 Pour ce faire, il suffit de récupérer le lien du fichier / vidéo / image et de le placer dans un fichiers "files.txt"
 
 
+### Fonctionnalités
+* téléchargement en automatique des fichiers en HTTP
+* relecture des éléments à télécharger tous les X secondes (paramètrable)
+* paramètrage du nombre de fichiers téléchargés à un instant T
+* édition du fichier de configuration via une interface web
+
 ### Utilisation
-* télécharger java 8 et installer java 8
+* télécharger java 8 et [installer java 8](http://www.java.com/fr/download/)
 * télécharger le programme  [ici](https://drive.google.com/open?id=0B3RZ6sP4kUBAeTh4eU5qbktEYm8)
-* alimenter le fichier files.txt à côté du programme java (*.jar)
+* alimenter le fichier files.txt à côté du programme java (*.jar) ou faite-le via l'interface web
+
 ```text
 la vidéo de mes vacances été 2015 :: http://sur-un-joli-site.com/mes-vacances15
+
 la vidéo de mes vacances été 2016 :: http://sur-un-joli-site.com/mes-vacances16
 ```
-* le programme va créer un répertoire "files/" qui contiendra les téléchargements
+* le programme va créer un répertoire **files/** qui contiendra les téléchargements (paramètrable)
+
+### Editer le fichier de configuration
+* Rendez-vous sur http://[adresse-de-mon-serveur]:[port]
+    * par défaut, le port est 4567 et si vous être sur votre machine "localhost" pour l'adresse du serveur
+
+### Command line interface
+
+Syntaxe : java [-options] -jar xxxxx.jar
+
+où options :
+    * -D
+        * conf : chemin vers le fichier de configuration, par défaut "./files.txt"
+        * files : chemin du répertoire de destination, par défaut "files/" qui sera créé automatique
+        * threads : nb. de threads qui vont ếté disponible pour traiter les téléchargements à un instant T, par défaut 3
+        * delay : interval de temps en secondes entre 2 relectures du fichier de configuration
+        * port : port du serveur web pour éditer le fichier de configuration, par défaut 4567
 
 ### Cas d'utilisation
 * télécharger régulièrement des fichier depuis internet (musiques, photos, vidéos)
-* installer ce programme sur un serveur, Raspberry...
+* installer ce programme sur un serveur, Raspberry... pour avoir des fichiers disponible...
 
 Licence MIT
--- src/main/java/com/github/ffremont/downloader/App.java
+++ src/main/java/com/github/ffremont/downloader/App.java
@@ -1,6 +1,7 @@
 package com.github.ffremont.downloader;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,6 +14,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
 
 /**
  * Hello world!
@@ -23,12 +27,14 @@ public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     private static List<String> downloading = new ArrayList<>();
+    private static Path conf;
 
     public static void main(String[] args) throws IOException {
-        Path conf = System.getProperty("conf") == null ? Paths.get("./files.txt") : Paths.get(System.getProperty("conf"));
-        Path films = System.getProperty("files") == null ? Paths.get("files") : Paths.get(System.getProperty("files"));
-        int threads = System.getProperty("threads") == null ? 3 : Integer.valueOf(System.getProperty("threads"));
-        int delay = System.getProperty("delay") == null ? 15 : Integer.valueOf(System.getProperty("delay"));
+        App.conf = System.getProperty("conf") == null ? Paths.get("files.txt") : Paths.get(System.getProperty("conf"));
+        final Path films = System.getProperty("files") == null ? Paths.get("files") : Paths.get(System.getProperty("files"));
+        final int threads = System.getProperty("threads") == null ? 3 : Integer.valueOf(System.getProperty("threads"));
+        final int delay = System.getProperty("delay") == null ? 15 : Integer.valueOf(System.getProperty("delay"));
+        final int port = System.getProperty("port") == null ? 4567 : Integer.valueOf(System.getProperty("port"));
 
         if (!Files.exists(films)) {
             Files.createDirectory(films);
@@ -60,6 +66,21 @@ public class App {
                 LOGGER.error("Impossible de récupérer la liste des fichiers", ex);
             }
         }, 0, delay, TimeUnit.SECONDS);
+        
+        port(port);
+        get("/", (request, response)-> {
+            return Thread.currentThread().getContextClassLoader().getResourceAsStream("configuration.html");
+        });
+        get("/conf", (request, response)-> {
+            return Files.newInputStream(App.conf.toAbsolutePath());
+        });
+        post("/conf", (request, response)-> {
+            synchronized(App.class){
+                Files.write(App.conf.toAbsolutePath(), request.bodyAsBytes());
+            }
+            
+            return "";
+        });
 
     }
 
